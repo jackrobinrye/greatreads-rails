b8306322d68b086fbff4f74236355406e3eea80f
Jack <jackrrye@gmail.com>
2019-12-05 13:17:11 -0600

    added sessions and static controllers through rails generators

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,15 @@
 Rails.application.routes.draw do
+  
+  get 'static/home'
+
+  get 'sessions/new'
+  post 'sessions/create'
+  get 'sessions/destroy'
+
   resources :genres
+
   resources :artists
+  
   resources :books
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
 end
