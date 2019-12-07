576d9a95409524e36985544defdc09b92337ac89
Jack <jackrrye@gmail.com>
2019-12-07 15:28:48 -0600

    started work on author crud

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -3,5 +3,27 @@ class AuthorsController < ApplicationController
     def show 
         @author = Author.find_by(id: params[:id])
     end 
+
+    def new 
+        @author = Author.new
+    end 
+
+    def create
+        @author = Author.new(author_params)
+        if @author.save
+            redirect_to author_path(@author)
+        else 
+            redirect_to new_author_path
+        end 
+    end 
+
+
+
+
+    private
+
+    def author_params
+        params.require(:author).permit(:name, :birthdate)
+    end 
     
 end
